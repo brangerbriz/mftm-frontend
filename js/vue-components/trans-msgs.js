02330dee477a6ecae3b9f8fe0988fd854d467d21
nbriz <nickbriz@gmail.com>
2018-04-17 11:27:08 -0500

    removed log

--- js/vue-components/trans-msgs.js
+++ js/vue-components/trans-msgs.js
@@ -122,7 +122,6 @@ Vue.component('trans-msgs', {
         },
         formatAnnotation:function(m){
             let a = m.annotation
-            console.log(a.indexOf('tx:'), a.length, a)
             if( a.indexOf('tx:')==0){
                 return null
             } else {
